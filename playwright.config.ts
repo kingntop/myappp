8d24ba01e339cac21470b371ac56e984c0dbce30
kingntop <kingntop@naver.com>
2022-08-06 02:43:06 +0900

    11

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 4,//process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -51,19 +51,19 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,17 +1,42 @@
-import { test, expect } from '@playwright/test';
+import {
+    test,
+    expect
+} from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test', async ({
+    page
+}) => {
 
-    await page.goto('https://playwright.dev/docs/api/class-playwright')
+    await page.goto('https://finance.yahoo.com/cryptocurrencies')
+    // #scr-res-table > div.Ovx\(a\).Ovx\(h\)--print.Ovy\(h\).W\(100\%\) > table > tbody > tr:nth-child(3) > td.Va\(m\).Ta\(start\).Px\(10px\).Fz\(s\)
+
+    const dataRows = await page.$$('#scr-res-table table > tbody > tr');
 
 
-    const dataRows = await page.$$('div.theme-doc-markdown.markdown > ul:nth-child(4) > li');
+    const urls = await page.$$eval('#scr-res-table table > tbody > tr a', (elements) =>
+        elements.map((el) => el.href),
+    )
 
-    console.log(dataRows.length)
+    // for (const url of urls) {
+    //     try {
+    //         await page.goto(url)
+    //         console.log(url)
+    //     } catch {}
+    // }
+    console.log(urls)
+    //      let posts = []
     for (let eachRow of dataRows) {
-        const variant_id = await eachRow.$('li');
-        console.log(variant_id)
+        const child1 = await (await (eachRow.$('td:nth-child(1)'))).textContent() || '';
+        const child2 = await (await (eachRow.$('td:nth-child(2)'))).textContent() || '';
+        const child3 = await (await (eachRow.$('td:nth-child(3)'))).textContent() || '';
+        const child4 = await (await (eachRow.$('td:nth-child(4)'))).textContent() || '';
+        // var v = await eachRow.evaluate( e => e.getAttribute( 'aria-label' ) );
+        const name = await eachRow.$eval('[aria-label="Name"]', el => el.textContent)
+        const price = await eachRow.$eval('[aria-label="Price (Intraday)"]', el => el.textContent)
+        // const name = await page.$eval('td.a\(m\).Ta\(start\).Px\(10px\).Fz\(s\)', el => el.textContent.trim())
+        // const locator = await page.locator('[aria-label="Name"]').getAttribute('content');
+
+        console.log( price, name)
     }
-    
 
 });
